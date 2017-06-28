abb2fef665c929bebc63f57b9ae8f18ad659265d
94004 <940043033@qq.com>
2017-06-28 22:29:20 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a94004/coolweather/gson/AQI.java
@@ -0,0 +1,15 @@
+package com.example.a94004.coolweather.gson;
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+
+}
